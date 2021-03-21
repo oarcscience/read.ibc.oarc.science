0f5a24954a1a550539dd7236821cecd02964aec5
- <->
2021-03-21 06:36:48 +0100

    edit

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'python', '0.0.1'
 gem 'jekyll', '3.7.2'
 
--- generatebase.py
+++ generatebase.py
@@ -1,16 +1,17 @@
 import pandas as pd
-import glob
+import os
 foutstring="filename,ls,lscum,firsts,content\n"
-for f in glob.glob("_data/sources getbible-net/*.txt"):
-    print("Working with file {} ...".format(f))
-    df=pd.read_csv(f ,sep="\|\|", engine="python", header=None)
-    df.columns=["book","chapter","verse","content"]
-    ls=",".join(map(str,list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len))))
-    lscum=",".join(map(str,[0]+list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len).cumsum())))
-    firsts=",".join(map(str,list(df[df["verse"]==1].index)))
-    content="".join(df.apply(lambda x: '"{}||{}||{} {}", '.format(x["book"],x["chapter"],x["verse"],x["content"]),axis=1))
-    foutstring+="'{}','{}','{}','{}','{}'\n".format(f,ls,lscum,firsts,content)
-    print("Done.")
+for f in os.listdir("_data/sources getbible-net/"):
+    if f.endswith(".txt") or filename.endswith(".py"): 
+        print("Working with file {} ...".format(f))
+        df=pd.read_csv(f ,sep="\|\|", engine="python", header=None)
+        df.columns=["book","chapter","verse","content"]
+        ls=",".join(map(str,list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len))))
+        lscum=",".join(map(str,[0]+list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len).cumsum())))
+        firsts=",".join(map(str,list(df[df["verse"]==1].index)))
+        content="".join(df.apply(lambda x: '"{}||{}||{} {}", '.format(x["book"],x["chapter"],x["verse"],x["content"]),axis=1))
+        foutstring+="'{}','{}','{}','{}','{}'\n".format(f,ls,lscum,firsts,content)
+        print("Done.")
 print("Writing down the final file...")
 with open("_data/globalbase.csv", "w") as fout:
     fout.write(foutstring)
