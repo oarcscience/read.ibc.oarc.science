fa4fd0ca5be38edf2c59ee800b2d91f169ea2c47
- <->
2021-03-21 06:02:52 +0100

    edit

---
+++ generatebase.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import glob
+foutstring="filename,ls,lscum,firsts,content\n"
+for f in glob.glob("_data/sources getbible-net/*.txt"):
+    print("Working with file {} ...".format(f))
+    df=pd.read_csv(f ,sep="\|\|", engine="python", header=None)
+    df.columns=["book","chapter","verse","content"]
+    ls=",".join(map(str,list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len))))
+    lscum=",".join(map(str,[0]+list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len).cumsum())))
+    firsts=",".join(map(str,list(df[df["verse"]==1].index)))
+    content="".join(df.apply(lambda x: '"{}||{}||{} {}", '.format(x["book"],x["chapter"],x["verse"],x["content"]),axis=1))
+    foutstring+="'{}','{}','{}','{}','{}'\n".format(f,ls,lscum,firsts,content)
+    print("Done.")
+print("Writing down the final file...")
+with open("_data/globalbase.csv", "w") as fout:
+    fout.write(foutstring)
+print("Done.")
