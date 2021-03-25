af678f7d59d52c6f1cc6137b9ae253ca5a7d2049
- <->
2021-03-25 18:39:15 +0100

    edit

--- generatebase.py
+++ generatebase.py
@@ -6,8 +6,8 @@ for f in glob.glob("_data/sources getbible-net/*.txt"):
     print("Working with file {} ...".format(f))
     df=pd.read_csv(f ,sep="\|\|", engine="python", header=None)
     df.columns=["book","chapter","verse","content"]
-    ls="¤".join(map(str,list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len))))
-    lscum="¤".join(map(str,[0]+list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len).cumsum())))
+    ls="¤".join(map(str,list(df[["book","chapter"]].groupby("book",sort=False).agg(set)["chapter"].apply(len))))
+    lscum="¤".join(map(str,[0]+list(df[["book","chapter"]].groupby("book",sort=False).agg(set)["chapter"].apply(len).cumsum())))
     firsts="¤".join(map(str,list(df[df["verse"]==1].index)+[df.shape[0]]))
     content="".join(df.apply(lambda x: '◊{}||{}||{} {}◊¤ '.format(x["book"],x["chapter"],x["verse"],x["content"]),axis=1))
     foutstring+="{},{},{},{},{}\n".format(f.split("/")[-1],ls,lscum,firsts,content.replace(",","¤"))
