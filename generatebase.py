b2c007473573105663fb94e6fd968cd35fd3b3f5
- <->
2021-03-25 13:23:08 +0100

    edit

--- generatebase.py
+++ generatebase.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import glob
+import csv
 foutstring="filename,ls,lscum,firsts,content\n"
 for f in glob.glob("_data/sources getbible-net/*.txt"):
     print("Working with file {} ...".format(f))
-    df=pd.read_csv(f ,sep="\|\|", engine="python", header=None)
+    df=pd.read_csv(f ,sep="\|\|", engine="python", header=None, quoting=csv.QUOTE_NONE)
     df.columns=["book","chapter","verse","content"]
     ls="¤".join(map(str,list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len))))
     lscum="¤".join(map(str,[0]+list(df[["book","chapter"]].groupby("book").agg(set)["chapter"].apply(len).cumsum())))
